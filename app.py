96a22fdc3b8710a04bc726b601298f6fd5b688bf
Naveenkumar1405 <121798977+Naveenkumar1405@users.noreply.github.com>
2024-02-20 15:27:49 +0530

    Mail Added

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import render_template, Flask, request
-import pywhatkit, datetime
+from flask import Flask, request, jsonify, render_template
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'navin.onwords@gmail.com'
+app.config['MAIL_PASSWORD'] = 'oizg ntdk nzsi csgo'
+mail = Mail(app)
 
 @app.route("/")
 def home():
@@ -20,21 +26,24 @@ def cars():
     return render_template('all-cars.html')
 
 @app.route('/send', methods=['POST'])
-def send_whatsapp_message():
+def send_message():
     name = request.form['Name']
     email = request.form['Email']
-    phone = request.form['phone']
     message = request.form['message']
+    phone = request.form['phone']
 
-    send_whatsapp_message(name, email, message, phone)
-    return 'successfull!'
+    if name and email and message:
+        try:
+            msg = Message('New Message From Website', sender=app.config['MAIL_USERNAME'], recipients=[app.config['MAIL_USERNAME']])
+            msg.body = f'Name: {name}\nPhone:{phone}\nEmail: {email}\nMessage: {message}'
+            mail.send(msg)
 
-def send_whatsapp_message(name, phone, email, message):
-    try:
-        now = datetime.datetime.now()
-        pywhatkit.sendwhatmsg("+919080517421", f"Name: {name}, Phone:{phone}, Email: {email}, Message: {message}", now.hour, now.minute + 1)
-    except Exception as e:
-        print(f"Failed to send message: {e}")
+            return jsonify({'success': True})
+        except Exception as e:
+            print(e)
+            return jsonify({'success': False}), 500
+    else:
+        return jsonify({'success': False}), 400
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80 ,debug=True)
