92c76dbd09a262d2fe5190effcde026e4fcaa0b3
Naveenkumar1405 <121798977+Naveenkumar1405@users.noreply.github.com>
2024-02-20 14:16:46 +0530

    Website completed

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def send_whatsapp_message(name, phone, email, message):
         print(f"Failed to send message: {e}")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080 ,debug=True)
+    app.run(host='0.0.0.0', port=80 ,debug=True)
