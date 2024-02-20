91a1fa0d611a73cae7a40ec18e4a1c52d76a208e
Naveenkumar1405 <121798977+Naveenkumar1405@users.noreply.github.com>
2024-02-20 16:15:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def send_message():
         return jsonify({'success': False}), 400
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080 ,debug=True)
+    app.run(host='0.0.0.0', port=80 ,debug=True)
